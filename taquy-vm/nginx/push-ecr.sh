d39c93a15dfc38ecc79180a4c1e12a7a710ecc80
taquy <quyt@unifiedpost.com>
2021-05-14 15:47:27 +0700

    update ngixn config, add fluentd to app stack

--- taquy-vm/nginx/push-ecr.sh
+++ taquy-vm/nginx/push-ecr.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PROJECT=fluentd
+PROJECT=kibana
 REGION=ap-southeast-1
 REPOSITORY_URI=397818416365.dkr.ecr.$REGION.amazonaws.com
 aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $REPOSITORY_URI --password-stdin
