2cc2435a63deec5c59d2b74a58708479eabda89f
taquy <quyt@unifiedpost.com>
2021-05-21 17:09:31 +0700

    add volume for app

--- taquy-vm/setup-app.sh
+++ taquy-vm/setup-app.sh
@@ -9,10 +9,16 @@ sysctl -w vm.max_map_count=262144
 docker network create -d bridge app
 
 # Create volume folder
-mkdir -p /data/es 
+mkdir -p /data/es
+mkdir -p /data/redis
 mkdir -p /data/mongo
-mkdir -p /data/redis 
 mkdir -p /data/tmp
+
+# Create docker volumes
+docker volume create --driver local --opt o=bind --opt type=none --opt device=/data/es es-vol
+docker volume create --driver local --opt o=bind --opt type=none --opt device=/data/redis redis-vol
+docker volume create --driver local --opt o=bind --opt type=none --opt device=/data/mongo mongo-vol
+docker volume create --driver local --opt o=bind --opt type=none --opt device=/data/tmp app-vol
 
 # ECR Login
 REGION=ap-southeast-1
