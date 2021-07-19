669d0e120027be9659271c9ec8993d34c12c5d6f
taquy <quyt@unifiedpost.com>
2021-07-19 20:17:23 +0700

    fix initial settings

--- infra/mssql/push-ecr.sh
+++ infra/mssql/push-ecr.sh
@@ -4,7 +4,6 @@ ACCOUNT=$(aws sts get-caller-identity | jq .Account -r )
 
 REGION=ap-southeast-1
 REPOSITORY_URI=$ACCOUNT.dkr.ecr.$REGION.amazonaws.com/mssql
-aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $REPOSITORY_URI --password-stdin
 
 echo "Repository for mssql: $REPOSITORY_URI"
 
@@ -13,6 +12,3 @@ docker tag  mcr.microsoft.com/mssql/server:2019-latest $REPOSITORY_URI
 
 echo "Start pushing mssql docker image to $REPOSITORY_URI"
 docker push $REPOSITORY_URI
-
-
-aws ecr get-login --no-include-email --registry-ids $ACCOUNT.dkr.ecr.$REGION.amazonaws.com/mssql --region ap--southeast-1
